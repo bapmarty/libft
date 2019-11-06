01321f9cade051f010b2c5bab77edb526a829da6
bapmarti <bapmarti@student.42.fr>
2019-11-06 15:31:45 +0100

    add 3 new functions

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:10:54 by bapmarti          #+#    #+#             */
-/*   Updated: 2019/11/06 13:13:59 by bapmarti         ###   ########.fr       */
+/*   Updated: 2019/11/06 15:26:14 by bapmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 # include <stdlib.h>
 
 void	ft_bzero(void *s, size_t n);
-void	ft_memset(void *s, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dst, void *src, size_t n);
+void	*ft_memccpy(void *dst, void *src, int c, size_t n);
+void	*ft_memchr(void *src, int c, size_t n);
 
 int		ft_atoi(char *s);
 int		ft_isalnum(int c);
