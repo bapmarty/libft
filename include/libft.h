cbb71859efd9b3b55e96e2f16740afdbb7674d69
bapmarti <bapmarti@student.42.fr>
2019-11-07 12:22:54 +0100

    Fix prototypes functions in libft.h

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:10:54 by bapmarti          #+#    #+#             */
-/*   Updated: 2019/11/06 15:26:14 by bapmarti         ###   ########.fr       */
+/*   Updated: 2019/11/07 12:00:59 by bapmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,26 @@
 
 # include <stdlib.h>
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memcpy(void *dst, void *src, size_t n);
-void	*ft_memccpy(void *dst, void *src, int c, size_t n);
-void	*ft_memchr(void *src, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void		*ft_memchr(const void *src, int c, size_t n);
 
-int		ft_atoi(char *s);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_strncmp(char *s1, char *s2, size_t n);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_strlen(char *s);
+char		*ft_strchr(const char *s, char c);
+char		*ft_strrchr(const char *s, char c);
 
-char	*ft_strchr(char *s, char c);
-char	*ft_strrchr(char *s, char c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+
+int			ft_atoi(const char *s);
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 
 #endif
