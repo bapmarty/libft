9910bd68b9335e583b551609ce43e37e19814371
bapmarti <bapmarti@student.42.fr>
2019-11-09 21:03:30 +0100

    Fix norm header

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:10:54 by bapmarti          #+#    #+#             */
-/*   Updated: 2019/11/09 13:23:48 by bapmarti         ###   ########.fr       */
+/*   Updated: 2019/11/09 21:03:05 by bapmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,31 @@
 
 # include <stdlib.h>
 
-void		ft_bzero(void *s, size_t n);
-void		*ft_memset(void *s, int c, size_t n);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		*ft_memmove(void *dst, const void *src, size_t n);
-void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void		*ft_memchr(const void *src, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memchr(const void *src, int c, size_t n);
 
-char		*ft_strchr(const char *s, char c);
-char		*ft_strrchr(const char *s, char c);
+char	*ft_strchr(const char *s, char c);
+char	*ft_strrchr(const char *s, char c);
+char	*ft_strnstr(const char *hs, const char *ne, size_t len);
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 
-int			ft_memcmp(const void *s1, const void *s2, size_t n);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
-int			ft_atoi(const char *s);
-int			ft_isalnum(int c);
-int			ft_isalpha(int c);
-int			ft_isascii(int c);
-int			ft_isdigit(int c);
-int			ft_isprint(int c);
-int			ft_toupper(int c);
-int			ft_tolower(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *s);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
