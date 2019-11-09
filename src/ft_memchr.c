62db473e2823541050aaf3802ddfe33b5d6c7b42
bapmarti <bapmarti@student.42.fr>
2019-11-09 20:17:42 +0100

    Fix ft_memchr

--- src/ft_memchr.c
+++ src/ft_memchr.c
@@ -6,20 +6,18 @@
 /*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 13:26:23 by bapmarti          #+#    #+#             */
-/*   Updated: 2019/11/07 12:55:03 by bapmarti         ###   ########.fr       */
+/*   Updated: 2019/11/09 20:15:30 by bapmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c_in, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char		c;
 	const unsigned char	*p;
 
-	c = c_in;
 	p = s;
-	while (*p != c && n-- > 0)
+	while (*p != (unsigned char)c && n-- > 0 && *p != '\0')
 		p++;
-	return (*p == c ? (void *)s : NULL);
+	return (*p == (unsigned char)c ? (void *)p : NULL);
 }
