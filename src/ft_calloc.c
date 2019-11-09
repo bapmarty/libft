ac00144c6f25af0c9e5f7347be794da08d96aab4
bapmarti <bapmarti@student.42.fr>
2019-11-09 19:56:54 +0100

    Add ft_calloc (tested)

---
+++ src/ft_calloc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/09 19:43:35 by bapmarti          #+#    #+#             */
+/*   Updated: 2019/11/09 19:55:36 by bapmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void *p;
+
+	if (!(p = malloc(count * size)))
+		return (NULL);
+	return (ft_memset(p, 0, count * size));
+}
