ff4600a0ed3ef7198d31d034c877d13137e07c94
bapmarti <bapmarti@student.42.fr>
2019-11-09 15:06:52 +0100

    Add ft_strlcat

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:10:54 by bapmarti          #+#    #+#             */
-/*   Updated: 2019/11/07 12:37:15 by bapmarti         ###   ########.fr       */
+/*   Updated: 2019/11/09 13:23:48 by bapmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@
 void		ft_bzero(void *s, size_t n);
 void		*ft_memset(void *s, int c, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t n);
 void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memchr(const void *src, int c, size_t n);
 
 char		*ft_strchr(const char *s, char c);
 char		*ft_strrchr(const char *s, char c);
 
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 
---
+++ src/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/09 13:10:17 by bapmarti          #+#    #+#             */
+/*   Updated: 2019/11/09 14:56:58 by bapmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t srclen;
+	size_t dstlen;
+	size_t i;
+
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	if (dstsize <= dstlen)
+		return (srclen + dstsize);
+	i = 0;
+	while (i < dstsize - dstlen - 1 && src[i])
+	{
+		dst[i + dstlen] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = '\0';
+	return (dstlen + srclen);
+}
