5680396c639af00db69e3ca473f2673b4d65ef85
bapmarti <bapmarti@student.42.fr>
2019-11-09 20:10:44 +0100

    Fix memcpy

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:35:58 by bapmarti          #+#    #+#             */
-/*   Updated: 2019/11/07 12:55:50 by bapmarti         ###   ########.fr       */
+/*   Updated: 2019/11/09 20:09:54 by bapmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char		*pdst;
-	const char	*psrc;
+	unsigned char		*pdst;
+	const unsigned char	*psrc;
 
 	pdst = dst;
 	psrc = src;
+	if (n == 0 || pdst == psrc)
+		return (dst);
 	while (n-- > 0)
 		*pdst++ = *psrc++;
 	return (dst);
