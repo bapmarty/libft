9aacce140027f4f63debd8cf33f89dbe2286fa92
bapmarti <bapmarti@student.42.fr>
2021-03-15 13:26:40 +0100

    :package: Add gnl header

---
+++ includes/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bapmarti <bapmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/22 17:59:31 by bapmarti          #+#    #+#             */
+/*   Updated: 2021/03/15 13:12:00 by bapmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# define BUFFER_SIZE 32
+
+int		get_next_line(int fd, char **line);
+
+#endif
